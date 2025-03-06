f5a0fd7999321d716976f26c4351a061d5c2ffcc
Loïc P <conctact@narigane.fr>
2025-03-06 11:33:11 +0100

    Add scheduler console and queue work

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('queue:work --timeout=60 --sleep=3',function (){
+    $this->info('Queue worker is running');
+})->purpose('Run the queue worker')->everyMinute()->withoutOverlapping();
